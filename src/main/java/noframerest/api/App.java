d3cae78e24b87bd388da2925e6fd2933960a5bf1
jethronap <efthioa@gmail.com>
2019-06-19 01:05:32 +0300

    read all entries from local json file

--- src/main/java/noframerest/api/App.java
+++ src/main/java/noframerest/api/App.java
@@ -1,32 +1,47 @@
 package noframerest.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpServer;
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import noframerest.model.User;
 
 /**
  *
  * @author jnap
  */
 public class App {
+
     public static void main(String[] args) throws IOException {
         int serverPort = 8000;
-        
+
+        ObjectMapper mapper = new ObjectMapper();
+
         HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
         server.createContext("/users/all", (exchange -> {
-            
-            if ("GET".equals(exchange.getRequestMethod())) {
-                String response = "these are all the users";
-                exchange.sendResponseHeaders(200, response.getBytes().length);
-                OutputStream output = exchange.getResponseBody();
-                output.write(response.getBytes());
-                output.flush();
-            } else {
-                exchange.sendResponseHeaders(405, -1); // HTTP Response Method not Allowed
+            try {
+                if ("GET".equals(exchange.getRequestMethod())) {
+                    
+                    //Json file to Java Object:
+                    User[] user = mapper.readValue(new File("/Volumes/flobmusic/_archives/code/Java/JavaExamples/indie/noFrameRestAPI/user.json"), User[].class);
+                    
+                    //Pretty print user from json file:
+                    String prettyUser = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(user);
+                    
+                    exchange.sendResponseHeaders(200, prettyUser.getBytes().length);
+                    OutputStream output = exchange.getResponseBody();
+                    output.write(prettyUser.getBytes());
+                    output.flush();
+                } else {
+                    exchange.sendResponseHeaders(405, -1); // HTTP Response Method not Allowed
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
             exchange.close();
-    }));
+        }));
         server.setExecutor(null); // creates a default executor
         server.start();
     }
