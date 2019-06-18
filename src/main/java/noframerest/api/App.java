b8c34ccbcaf2d9aa59dc9d93059ff5b21369a4c2
jethronap <efthioa@gmail.com>
2019-06-18 20:23:44 +0300

    server started at localhost:8000

--- src/main/java/noframerest/api/App.java
+++ src/main/java/noframerest/api/App.java
@@ -1,11 +1,33 @@
 package noframerest.api;
 
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 /**
  *
  * @author jnap
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        int serverPort = 8000;
         
+        HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
+        server.createContext("/users/all", (exchange -> {
+            
+            if ("GET".equals(exchange.getRequestMethod())) {
+                String response = "these are all the users";
+                exchange.sendResponseHeaders(200, response.getBytes().length);
+                OutputStream output = exchange.getResponseBody();
+                output.write(response.getBytes());
+                output.flush();
+            } else {
+                exchange.sendResponseHeaders(405, -1); // HTTP Response Method not Allowed
+            }
+            exchange.close();
+    }));
+        server.setExecutor(null); // creates a default executor
+        server.start();
     }
 }
