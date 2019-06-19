2cde6fcc95f2e1837bab7eb3c565eca880e54377
jethronap <efthioa@gmail.com>
2019-06-19 15:32:58 +0300

    created newUser to post

---
+++ src/main/java/noframerest/model/NewUser.java
@@ -0,0 +1,22 @@
+package noframerest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author jnap
+ */
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class NewUser {
+    private String id;
+    private String username;
+    private String password;
+}
